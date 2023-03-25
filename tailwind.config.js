dfe9a6ae7e88f09e6ff8c365e6faba25edc9d59c
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-03-26 02:54:05 +0300

    code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      greenLinear: "#33D35E",
-      blueLinear: "#2AB6D9",
-      greenlight: "#30C88F",
-      bluelight: "#2D314D",
-      graylight: "#9597A5",
-      white: "#FFFFFF",
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        greenLinear: "#33D35E",
+        blueLinear: "#2AB6D9",
+        greenlight: "#30C88F",
+        bluelight: "#2D314D",
+        graylight: "#9597A5",
+        white: "#FFFFFF",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("./plugins/openVarient")],
 };
